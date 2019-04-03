476ac1f8a79e44b6fc1e6337e22fc69def09b8d2
indhumathi1910 <33888855+indhumathi1910@users.noreply.github.com>
2019-04-03 14:46:49 +0530

    Add files via upload

---
+++ OrderController.java
@@ -0,0 +1,70 @@
+package com.ecomm.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.pro.dao.CartDao;
+import org.pro.dao.OrderDao;
+import org.pro.dao.UserDao;
+import org.pro.model.Cart;
+import org.pro.model.OrderDetail;
+import org.pro.model.UserDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OrderController 
+{
+@Autowired
+CartDao cartDao;
+
+@Autowired
+OrderDao orderDao;
+
+@Autowired
+UserDao userDao;
+
+@RequestMapping(value="/pay")
+public String PaymentProcess(@RequestParam("PaymentMode")String paymentMode,Model l,HttpSession session)
+{
+	String UserName=(String)session.getAttribute("Username");
+	List<Cart> listcartitems=cartDao.listcartitems(UserName);
+	
+	OrderDetail orderdetail=new OrderDetail();
+	orderdetail.setUserName(UserName);
+	orderdetail.setOrderDate(new Date());
+	orderdetail.setPaymentmode(paymentMode);
+	orderdetail.setTotalAmount(this.calcGrandtotal(cartDao.listcartitems(UserName)));
+	l.addAttribute("labelcart",cartDao.listcartitems(UserName));
+	l.addAttribute("paymentDetail",orderdetail);
+	
+     if(orderDao.UpdatecartitemStatus(UserName))
+	{
+		orderDao.PaymentProcess(orderdetail);
+		l.addAttribute("labelcart",listcartitems);
+		l.addAttribute("paymentDetail",orderdetail);
+		UserDetail userd=userDao.getusers(UserName);
+	     l.addAttribute("userdetail",userd);
+	}
+	return "Receipt";
+}
+public int calcGrandtotal(List<Cart> listcartitems)
+{
+	int grandtotal=0;
+	int count=0;
+	while(count<listcartitems.size())
+	{
+	Cart cart=listcartitems.get(count);
+	grandtotal=grandtotal+(cart.getQuantity() * cart.getPrice());
+	count++;
+	}
+
+  return grandtotal;
+}
+}
---
+++ index.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<a href="/login">Login</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="/register">Register</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="/contactus">Category</a>&nbsp;&nbsp;|&nbsp;&nbsp;
+<a href="/product">Product</a>
+</body>
+</html>
